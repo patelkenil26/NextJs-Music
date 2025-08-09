1dcb8e2be8b76cb2506ff75ce66da4b13a1d2804
bhoomik03 <bhoomikpatel25@gmail.com>
2025-08-09 17:16:47 +0530

    update featuredCourses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
-      <FeaturedCourses />
+      <FeaturedCourses /> 
       <WhyChooseUs/>
       <MusicSchoolTestimonial/>
       <UpcommingWebinars/>
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 import React from "react";
 import courseData from "../data/music_courses.json";
 import Link from "next/link";
-import { BackgroundGradient } from "./ui/background-gradient"
+import { BackgroundGradient } from "./ui/background-gradient";
 import { Button } from "./ui/moving-border";
 
-
-interface Course{
-  id: number,
-  title: string,
-  slug: string,
-  description: string,
-  price: number,
-  instructor: string,
-  isFeatured: boolean,
-      
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
 }
 
 const FeaturedCourses = () => {
-  const featuredCourses = courseData.courses.filter((course:Course) => course.isFeatured)
+  const featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
   return (
     <div className="py-12 bg-gray-900">
       <div className="text-center">
@@ -30,35 +30,34 @@ const FeaturedCourses = () => {
         </p>
       </div>
       <div className="mt-10 mx-8">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-                {featuredCourses.map((course:Course)=> (
-                    <div key={course.id} className="flex justify-center">
-                        <BackgroundGradient
-                        className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
-                            <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
-                                <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
-                                <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
-                                <Link href={`/courses/${course.slug}`}>
-                                Learn More
-                                </Link>
-                            </div>
-                        </BackgroundGradient>
-                    </div>
-                ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className="flex justify-center">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
+                </div>
+              </BackgroundGradient>
             </div>
+          ))}
         </div>
+      </div>
       <div className="mt-20 text-center">
-            <Link href={"/courses"}
-            >
-              <Button
-                            borderRadius="1.75rem"
-                            className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                          >
-                            View All courses
-                          </Button>
-            
-            </Link>
-        </div>
+        <Link href={"/courses"}>
+          <Button
+            borderRadius="1.75rem"
+            className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            View All courses
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
